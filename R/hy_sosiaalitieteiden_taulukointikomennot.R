38575a358ce26a73d7fc40b27ae4b6842f534230
arhot <arho.toikka@helsinki.fi>
2022-08-29 11:27:44 +0300

    First working version.

--- R/hy_sosiaalitieteiden_taulukointikomennot.R
+++ R/hy_sosiaalitieteiden_taulukointikomennot.R
@@ -1,34 +1,50 @@
+#' @import dplyr
+#' @import psych
+#' @import forcats
+#' @import janitor
+#' @import broom
+
+#' @title Faktorianalyysin tulostaulukko
+#'
+#' @description Funktio tekee psych-paketin faktorianalyysimalli HY:n sosiaalitieteiden koulutusohjelmien ohjeiden mukaisen tulostaulukon.
+#' @param faktorimalli faktorianalyysimalli
+#' @param kieli Jos suomi, kääntää käsitteet suomeksi, muuten jättää englanninkieliset.
+#' @param desimaalierotin Jos pilkku, muuttaa valmiin taulukon desimaalierottimen pilkuksi, muuten jättää alkuperäisen pisteen.
+#' @param piilota_lataukset_alle Poistaa taulukosta argumentin itseisarvoa pienemmät lataukset.
+#' @return Valmis faktorianalyysitaulukko.
+
 taulukoi_faktorianalyysi <- function(faktorimalli, kieli="suomi", desimaalierotin="piste", piilota_lataukset_alle = 0.3) {
-  
-  taulukko1 <- faktorimalli$loadings  %>% fa.sort() %>% unclass() %>% as_tibble(rownames ="Muuttuja") %>% 
-    mutate(across(where(is.numeric), ~case_when(abs(.x) < piilota_lataukset_alle  ~ "", TRUE ~ round_half_up(.x, 2) %>% 
-                                                  format(nsmall=2)))) %>% 
+  if (class(malli1_faktorianalyysi)[1] != "psych") {stop("Ensimmäisen argumentin tulee olla faktorianalyssin tulosobjekti.")}
+
+  taulukko1 <- faktorimalli$loadings  %>% fa.sort() %>% unclass() %>% as_tibble(rownames ="Muuttuja") %>%
+    mutate(across(tidyselect::vars_select_helpers$where(is.numeric), ~case_when(abs(.x) < piilota_lataukset_alle  ~ "", TRUE ~ round_half_up(.x, 2) %>%
+                                                  format(nsmall=2)))) %>%
     left_join(faktorimalli$communalities %>% as_tibble(rownames="Muuttuja") %>%
                 mutate(Kommunaliteetti = round_half_up(value,2), .keep="unused"))
-  
-  taulukko2 <- faktorimalli$Vaccounted %>% as_tibble(rownames = "Muuttuja") %>% 
-    mutate(across(where(is.numeric), ~round_half_up(.x, 2) %>% format(nsmall=2))) %>% 
+
+  taulukko2 <- faktorimalli$Vaccounted %>% as_tibble(rownames = "Muuttuja") %>%
+    mutate(across(tidyselect::vars_select_helpers$where(is.numeric), ~round_half_up(.x, 2) %>% format(nsmall=2))) %>%
     filter(Muuttuja == "SS loadings" | Muuttuja == "Proportion Var")
-  
+
   if(kieli=="suomi"){
     taulukko2 <- taulukko2 %>% mutate(Muuttuja = fct_recode(Muuttuja, "Ominaisarvo" = "SS loadings", "Selitysosuus %" = "Proportion Var"))
   }
-  
-  valmis_taulukko <- taulukko1 %>% bind_rows(taulukko2) 
-  
+
+  valmis_taulukko <- taulukko1 %>% bind_rows(taulukko2)
+
   if(desimaalierotin=="pilkku"){
     valmis_taulukko <- valmis_taulukko %>% mutate(across(everything(), ~str_replace_all(.x, "\\.", ",")))
   }
-  
+
   valmis_taulukko
 }
 
 tee_korrelaatiotaulukko <- function(korrelaatiomatriisi) {
   korrelaatio_arvot <- korrelaatiot$r %>%  as_tibble() %>% mutate_if(is.numeric, round, 2) %>% as.matrix()
-  korrelaatioiden_p_arvot <- korrelaatiot$p %>%  as_tibble() %>%  mutate_if(is.numeric, 
-                                                                            cut, breaks=c(-0.1,0.001,0.01,0.05,1), 
+  korrelaatioiden_p_arvot <- korrelaatiot$p %>%  as_tibble() %>%  mutate_if(is.numeric,
+                                                                            cut, breaks=c(-0.1,0.001,0.01,0.05,1),
                                                                             labels=c("***", "**", "*", "")) %>% as.matrix()
-  korrelaatiotaulukko <- matrix(paste(korrelaatio_arvot,korrelaatioiden_p_arvot, sep=""), dim(korrelaatio_arvot)[1], dim(korrelaatio_arvot)[1]) 
+  korrelaatiotaulukko <- matrix(paste(korrelaatio_arvot,korrelaatioiden_p_arvot, sep=""), dim(korrelaatio_arvot)[1], dim(korrelaatio_arvot)[1])
   korrelaatiotaulukko[lower.tri(korrelaatiotaulukko,diag=TRUE)] <- NA
   colnames(korrelaatiotaulukko) <- colnames(korrelaatiot$r)
   rownames(korrelaatiotaulukko) <- colnames(korrelaatiot$r)
@@ -37,28 +53,33 @@ tee_korrelaatiotaulukko <- function(korrelaatiomatriisi) {
 }
 
 tee_regressiotaulukko<- function(malli) {
-  taulukko <- tidy(malli, conf.int=T) %>% add_column(beta = lm.beta(malli)$standardized.coefficients) %>% 
-    select(Muuttuja=term, B=estimate, LV_alaraja=conf.low, LV_ylaraja=conf.high, beta, p=p.value)  %>% 
-    mutate_if(is.numeric, round, 3) %>% mutate(p= cut(p, breaks=c(-0.1,0.001,0.01,0.05,1), 
-                                                      labels=c("***", "**", "*", ""))) %>% 
+  taulukko <- tidy(malli, conf.int=T) %>% tibble::add_column(beta = lm.beta::lm.beta(malli)$standardized.coefficients) %>%
+    select(Muuttuja=term, B=estimate, LV_alaraja=conf.low, LV_ylaraja=conf.high, beta, p=p.value)  %>%
+    mutate_if(is.numeric, round, 3) %>% mutate(p= cut(p, breaks=c(-0.1,0.001,0.01,0.05,1),
+                                                      labels=c("***", "**", "*", ""))) %>%
     mutate(Luottamusvali = paste("[", LV_alaraja, ",", LV_ylaraja, "]"), .after=B) %>% select(-LV_alaraja, -LV_ylaraja) %>% filter(Muuttuja!= "(Intercept)")
   selitysosuus <- summary(malli)$adj.r.squared %>% round_half_up(3) %>% as_tibble(rownames="Muuttuja") %>% rename(B=value) %>%
     mutate(Muuttuja = "R^2^, korjattu")
-  
+
   bind_rows(taulukko, selitysosuus)
 }
 
 tee_regressiotaulukko_selitettava_rivina <- function(malli) {
-  selitettava <- all.vars(terms(malli))[1]
-  taulukko <- tidy(malli, conf.int=T) %>% add_column(beta = lm.beta(malli)$standardized.coefficients) %>% 
-    add_column(Selitettava = selitettava) %>% 
-    select(Selitettava, Muuttuja=term, B=estimate, LV_alaraja=conf.low, LV_ylaraja=conf.high, beta, p=p.value)  %>% 
-    mutate_if(is.numeric, round, 3) %>% mutate(p= cut(p, breaks=c(-0.1,0.001,0.01,0.05,1), 
-                                                      labels=c("***", "**", "*", ""))) %>% 
+  selitettava <- all.vars(stats::terms(malli))[1]
+  taulukko <- tidy(malli, conf.int=T) %>% tibble::add_column(beta = lm.beta::lm.beta(malli)$standardized.coefficients) %>%
+    tibble::add_column(Selitettava = selitettava) %>%
+    select(Selitettava, Muuttuja=term, B=estimate, LV_alaraja=conf.low, LV_ylaraja=conf.high, beta, p=p.value)  %>%
+    mutate_if(is.numeric, round, 3) %>% mutate(p= cut(p, breaks=c(-0.1,0.001,0.01,0.05,1),
+                                                      labels=c("***", "**", "*", ""))) %>%
     mutate(Luottamusvali = paste("[", LV_alaraja, ",", LV_ylaraja, "]"), .after=B) %>% select(-LV_alaraja, -LV_ylaraja) %>% filter(Muuttuja!= "(Intercept)")
   selitysosuus <- summary(malli)$adj.r.squared %>% round_half_up(3) %>% as_tibble(rownames="Muuttuja") %>% rename(B=value) %>%
     mutate(Muuttuja = "R^2^, korjattu")
-  
+
   bind_rows(taulukko, selitysosuus)
 }
+
+
+if(getRversion() >= "2.15.1")  utils::globalVariables(c("value", "Muuttuja", "korrelaatiot", "term", "estimate",
+                                                        "conf.low", "conf.high", "p.value", "p", "LV_alaraja", "LV_ylaraja",
+                                                        "B", "Selitettava", "term"))
 
